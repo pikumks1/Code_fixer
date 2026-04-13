a058e280709182977cd4b7f5986ed0ff53d6b95a
Manish <pikumks@gmail.com>
2026-04-13 19:50:32 +0530

    monaco editor and theme added

--- scripts_to_fix/test.js
+++ scripts_to_fix/test.js
@@ -9,6 +9,12 @@ function UpdateAccount() {
     /*var m1 = 10, m2, m3=20, m4="", m5 = null;*/
     var n1 = 10; /*n3=20, n4="", n5 = null;*/ var n6="20";
     var o1 = 10; //no=20=null;
+
+        //var psInputs = ""; var psOutputs = "";
+var lov = TheApplication().InvokeMethod("LookupValue","TEST_LOV","TEST_LOV1");
+var testBSInv = vcReadFile.InvokeMethod("ReadEAIMsg", psInputs, psOutputs);
+if (var i=0; i>10; i++)
+    
     try {
         var x="";
         var x="", y="12", z="", a="";
